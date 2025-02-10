0547ce16b84dee6fc0eb222016bac53149e3f4ac
m.fukazawa <mfukazawa@digitalvision.co.jp>
2025-02-10 16:00:28 +0900

    サンプルをコミット

---
+++ src/main/java/com/example/taskmate/entity/Status.java
@@ -0,0 +1,15 @@
+package com.example.taskmate.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Status {
+
+	private String statusCode;
+	private String statusName;
+	
+}
---
+++ src/main/java/com/example/taskmate/java_sampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class java_sampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(java_sampleApplication.class, args);
+	}
+
+}
